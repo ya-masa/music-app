3b3b7792c950bfa2a55d7a9fe51e2dfdff962367
ya-masa <smile.iwa.65@gmail.com>
2026-05-01 22:26:26 +0900

    Re23

--- js/app.js
+++ js/app.js
@@ -340,7 +340,7 @@ function renderSongList(songs, targetId, allSongs) {
     `;
 
     // 再生
-    div.addEventListener("click", (e) => {
+    div.querySelector(".song-item").addEventListener("click", () => {
       // 保存ボタンや削除ボタンを押したときは再生しない
       if (e.target.closest(".save-btn") || e.target.closest(".delete-btn")) return;
 
