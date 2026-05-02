a0f730382bffa23deddf11bfbe405cd746c46d11
ya-masa <smile.iwa.65@gmail.com>
2026-05-02 11:10:14 +0900

    re20

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ async function getOfflineSongs() {
       songs.push({
         id,
         name,
-        url: `/music-app/offline/${encodeURIComponent(id + "__" + name)}`,
+        url: url,
         offline: true
       });
     }
